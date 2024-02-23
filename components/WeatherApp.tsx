67f7fa096b526ba0efa721d680f757d950bb9ef1
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-02-23 19:57:05 +0530

    update: audit push from local env

--- components/WeatherApp.tsx
+++ components/WeatherApp.tsx
@@ -1,12 +1,31 @@
+'use client'
+
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { FaWind, FaWater } from "react-icons/fa6";
+
+// export const getServerSideProps = () => {
+//   console.log(process.env.API_KEY);
+//   return {
+//     props:{
+//       env: 'private api keys',
+//     },
+//   }
+// }
 
 export default function WeatherApp () {
 
+  const detailClassName = "flex items-center justify-center space-x-3"
+  const weatherAPI = process.env.API_KEY
+  console.log(weatherAPI)
+
+  // https://api.weatherapi.com/v1/current.json?key=3d3dd43aa77149eaa8d15206232512&q=London&aqi=no
+
+
   return (
-    <section className="h-[550px] w-[420px] bg-gradient-to-b from-[#4B14D3] to-[#353C91] shadow-xl rounded-xl px-10 py-10 flex flex-col items-center justify-between">
+    <section className="h-[550px] w-[420px] bg-gradient-to-b from-[#4B14D3] to-[#353C91] shadow-2xl rounded-xl px-10 py-10 flex flex-col items-center justify-between">
       
       <div className="flex space-x-5 w-full">
-        <input type="text" className='w-full rounded-xl text-black px-3 font-medium'/> 
+        <input type="text" placeholder='Search' className='w-full rounded-xl text-black px-3 font-medium'/> 
         <button>
           <MagnifyingGlassIcon className='text-black h-10 p-2 bg-white rounded-[50%] hover:bg-gray-200'/>
         </button>
@@ -16,20 +35,26 @@ export default function WeatherApp () {
 
       <h1 className='text-7xl font-medium'>24°C</h1>
 
-      <h3 className='text-4xl'>City, Country</h3>
+      <h3 className='text-4xl'>London, UK</h3>
+    
+        <div className="flex justify-between w-full">
       
-      <div className="flex justify-between w-full">
-        <div>
-          <p>64%</p>
-          <p>Humidity</p>
-        </div>
+          <div className={detailClassName}>
+            <FaWater className='size-[35px]'/>
+            <div>
+              <p className='text-2xl'>64%</p>
+              <p>Humidity</p>
+            </div>
+          </div>
 
-        <div>
-          <p>18km/h</p>
-          <p>Wind Speed</p>
+          <div className={detailClassName}>
+            <FaWind className='size-[35px]'/>
+            <div>
+              <p className='text-2xl'>18 km/h</p>
+              <p>Wind Speed</p>
+            </div>
+          </div>
         </div>
-        
-      </div>
     </section>
   )
 }
