95bdb3318e6ed55237aef0f6007038e9d569de52
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-02-23 18:07:42 +0530

    update: weatherApp UI

--- components/WeatherApp.tsx
+++ components/WeatherApp.tsx
@@ -1,19 +1,34 @@
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+
 export default function WeatherApp () {
+
   return (
     <section className="h-[550px] w-[420px] bg-gradient-to-b from-[#4B14D3] to-[#353C91] shadow-xl rounded-xl px-10 py-10 flex flex-col items-center justify-between">
       
-      <div className="flex">
-        <input type="text" /> <button>S</button>
+      <div className="flex space-x-5 w-full">
+        <input type="text" className='w-full rounded-xl text-black px-3 font-medium'/> 
+        <button>
+          <MagnifyingGlassIcon className='text-black h-10 p-2 bg-white rounded-[50%] hover:bg-gray-200'/>
+        </button>
       </div>
       
-      <img src="/cloudy.png" alt="weatherSvg" className="h-[150px]" />
-      // to fix weather svg size
-      <h1>temperature</h1>
+      <img src="/cloudy.png" alt="weatherSvg" className="h-[150px] aspect-square" />
 
-      <h3>City, Country</h3>
+      <h1 className='text-7xl font-medium'>24°C</h1>
+
+      <h3 className='text-4xl'>City, Country</h3>
       
-      <div className="flex">
-        <p>humidity %</p> <p>wind speed km/h</p>
+      <div className="flex justify-between w-full">
+        <div>
+          <p>64%</p>
+          <p>Humidity</p>
+        </div>
+
+        <div>
+          <p>18km/h</p>
+          <p>Wind Speed</p>
+        </div>
+        
       </div>
     </section>
   )
