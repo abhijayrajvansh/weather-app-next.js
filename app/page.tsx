d45e60d26042b561d07d96cae48bd1b79ed87deb
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2024-02-24 19:17:34 +0530

    axios.get{api} && conditional execution

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import WeatherApp from "@/components/WeatherApp";
 
 export default function page () {
   return (
-    <WeatherApp />
+    <>
+      <WeatherApp />
+    </>
   )
 }
--- components/WeatherApp.tsx
+++ components/WeatherApp.tsx
@@ -3,74 +3,87 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 import { FaWind, FaWater } from "react-icons/fa6";
 import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface Location {
+  name: string;
+  country: string;
+}
+
+interface Condition {
+  icon: string;
+}
+
+interface Current {
+  temp_c: number;
+  humidity: number;
+  wind_kph: number;
+  condition: Condition;
+}
+
+interface WeatherDataProps {
+  location: Location;
+  current: Current;
+}
 
 export default function WeatherApp () {
 
-  const [searchQuery, setSearchQuery] = useState('London')
-  
+  const API_KEY = '3d3dd43aa77149eaa8d15206232512'
   // warning: init api key using backend and fetch data from server without showing api key on client side
   // const weatherAPI_KEY = process.env.API_KEY
   // let weather_API_URL = `https://api.weatherapi.com/v1/current.json?key=${weatherAPI_KEY}&q=${searchQuery}&aqi=no`
   
-  const weather_API_URL = `https://api.weatherapi.com/v1/current.json?key=3d3dd43aa77149eaa8d15206232512&q=${searchQuery}&aqi=no`
+  const [searchQuery, setSearchQuery] = useState<string>('')
+  const [weatherData, setWeatherData  ] = useState<WeatherDataProps | null>(null);
 
-  const [fulfilledData, setFulfilledData  ] = useState([]);
-  console.log(fulfilledData)
+  
+  const getWeatherData = async (cityName: string) => {
+    try {
+      const res = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${cityName}&aqi=no`)
+      setWeatherData(res.data)
+    }
+    catch (error) {
+      console.log('oops, caught some error!')
+    } 
+  }
+
+  useEffect (() => {
+    getWeatherData('London')
+    // get executed on mount
+  }, [])
 
   function handleClick () {
     console.log(`fetching weather_API_URL using ${searchQuery}`)
+    getWeatherData(searchQuery)
   }
 
-  useEffect(() => {
-    fetch(weather_API_URL).then(res => res.json()).then(e => setFulfilledData(e))
-
-  }, []);
-
-  const DEBUG = true
-  if(DEBUG) {
-    return <div>
-      <h1>Debug Mode</h1>
-
-
-    </div>
-  }
   
   return (
     <section className="h-[550px] w-[390px] bg-gradient-to-b from-[#4B14D3] to-[#353C91] shadow-2xl rounded-xl px-10 py-10 flex flex-col items-center justify-between">
       <div className="flex space-x-5 w-full">
-        
-        <input type="text" onChange={queryValue => setSearchQuery(queryValue.target.value)} value={searchQuery} className='w-full rounded-xl text-black px-3 font-medium'/> 
-        
+        <input type="text" placeholder='Enter city or country...' onChange={queryValue => setSearchQuery(queryValue.target.value)} value={searchQuery} className='w-full rounded-xl text-gray-800 px-3 font-medium bg-white bg-opacity-[100%]'/> 
         <button>
           <MagnifyingGlassIcon onClick={handleClick} className='text-black h-10 p-2 bg-white rounded-[50%] hover:bg-gray-200'/>
         </button>
-        
       </div>
-      
-      <img src="/cloudy.png" alt="weatherSvg" className="h-[150px] aspect-square" />
-      
-      <h1 className='text-7xl font-medium'>24°C</h1>
-      
-      <h3 className='text-4xl'>London, UK</h3>
-        
+      <img src={weatherData?.current.condition.icon} alt="weatherSvg" className="h-[150px]" />
+      <h1 className='text-6xl font-medium'>{weatherData?.current.temp_c}°C</h1>
+      <h3 className='text-3xl text-center'>{weatherData?.location.name}, {weatherData?.location.country}</h3>
         <div className="flex justify-between w-full">      
-         
          <div className="flex items-center justify-center space-x-3">
             <FaWater className='size-[35px]'/>
             <div>
-              <p className='text-2xl'>64%</p>
+              <p className='text-2xl'>{weatherData?.current.humidity}%</p>
               <p>Humidity</p>
             </div>
-          
           </div>
           <div className="flex items-center justify-center space-x-3">
             <FaWind className='size-[35px]'/>
             <div>
-              <p className='text-2xl'>18 km/h</p>
+              <p className='text-2xl'>{weatherData?.current.wind_kph} km/h</p>
               <p>Wind Speed</p>
             </div>
           </div>
-        
         </div>
     </section>
   )
